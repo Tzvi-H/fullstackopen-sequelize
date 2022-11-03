f61dc11f5ae55e7973a9535aa10b97d878501347
Tzvi-H <16guitar@gmail.com>
2022-11-03 11:09:01 -0600

    13.a

--- theory/index.js
+++ theory/index.js
@@ -1,26 +1,82 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
-const user = process.env.user;
-const host = process.env.host;
-const database = process.env.database;
-const password = process.env.password;
-const port = process.env.port;
+app.use(express.json());
+
+const { user, host, database, password, port } = process.env;
 
 const sequelize = new Sequelize(database, user, password, {
   host,
   port,
   dialect: "postgres",
-  logging: false,
+  // logging: false,
 });
 
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+Note.sync();
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll();
+  console.log(JSON.stringify(notes, null, 2));
+
   res.json(notes);
+});
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+app.post("/api/notes", async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    try {
+      await note.save();
+      res.json(note);
+    } catch (error) {
+      return res.status(400).json({ error });
+    }
+  } else {
+    res.status(404).end();
+  }
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
